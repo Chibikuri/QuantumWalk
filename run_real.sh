77a3d68f6df19cf7c7291ed8befacfabb27270e0
Chibikuri <ryosuke.satoh.july@gmail.com>
2018-11-27 14:57:12 +0900

    add data

---
+++ run_real.sh
@@ -0,0 +1,3 @@
+python QuantumWalk.py 5 10
+python QuantumWalk.py 7 10
+python QuantumWalk.py 11 10
--- run_sim.sh
+++ run_sim.sh
@@ -1,3 +1,3 @@
-python QuantumWalk.py 3
-python QuantumWalk.py 9
-python QuantumWalk.py 11
+python QuantumWalk.py 5 1
+python QuantumWalk.py 7 1
+python QuantumWalk.py 11 1
